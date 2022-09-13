efaf1b1723ab6713923ea72a212ad64d78d50124
moifore <moiforejallah@gmail.com>
2022-09-13 11:53:22 -0700

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry write a program that prints _putchar followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+		printf("_putchar");
+		printf("\n");
+		return (0);
+}
+
