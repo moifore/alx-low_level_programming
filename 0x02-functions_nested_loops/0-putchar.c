4377f2467dce92b9aff38a292978c5a15bcefa98
moifore <moiforejallah@gmail.com>
2022-09-13 12:18:08 -0700

    Script that prints _putchar with new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - Prints _putchar with new line
  *
  * Return: Always 0 (Success)
  */
 int main(void)
-	int main(void)
 {
 		_putchar('_');
 		_putchar('p');
@@ -16,6 +15,7 @@ int main(void)
 		_putchar('a');
 		_putchar('r');
 		_putchar('\n');
+
 		return (0);
 }
 
