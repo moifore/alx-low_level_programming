cf9951011db2e8d70423ead20e84201b29644274
moifore <moiforejallah@gmail.com>
2022-09-30 13:02:51 -0700

    prints its name, followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - prints the program
+ * main - prints the name of the program passed to argv
  * @argc: total argument passed to array
  * @argv: argument vector pointing to array of strings
- * Return 0 success
+ * Return: Always  0 Success
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
