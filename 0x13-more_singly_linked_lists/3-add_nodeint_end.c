2ac7e67edf9ba014b54a9da13783f28634ccb0a1
moifore <moiforejallah@gmail.com>
2022-10-25 07:48:18 -0700

    Function that adds a new node at the end of a linked list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -5,6 +5,7 @@
  *
  * @head: A pointer to the address of the head of linked list
  * @n: Integer for the new node
+ * Return: An integer at the address of the last node
  */
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
