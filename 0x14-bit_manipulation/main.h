c425b27ef69f4414eb3a72968d42f04cd97a3b70
moifore <moiforejallah@gmail.com>
2022-10-27 05:00:59 -0700

    Header file with all prototypes of the project

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * main - header file that contains prototypes for all functions
+ * Desc: Prototypes for all functions written for this project
+ */
+
+int _putchar(char c);
+unsigned int binary_to_unit(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+
+#endif /* MAIN_H */
